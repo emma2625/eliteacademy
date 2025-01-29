151b544bbcbc6f6c2ebe82cef7a77fec2b46a169
emma2625 <emmanuelodobo10@gmail.com>
2025-01-29 16:03:19 +0100

    Updated

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -6,13 +6,15 @@
             @csrf
             <div class="col-md-4 border-right">
                 <div class="d-flex flex-column align-items-center text-center p-3 py-5">
-                    <img class="rounded-circle mt-5" width="150px"
-                        src="{{ asset('profile.jpg') }}">
+                    <img class="rounded-circle mt-5" width="150px" src="{{ asset('profile.jpg') }}">
                     <span class="font-weight-bold"> {{ Auth::user()->name }} </span>
                     <span class="text-black-50"> {{ Auth::user()->email }} </span>
                     <span> </span>
 
-                    <input type="file" class="form-control form-control-sm" accept="image/*">
+                    <input type="file" name="picture" class="form-control form-control-sm" accept="image/*">
+                    @error('picture')
+                        <small class="text-danger font-bold">{{ $message }}</small>
+                    @enderror
                 </div>
             </div>
             <div class="col-md-8 border-right">
@@ -23,25 +25,40 @@
                     <div class="row mt-2">
                         <div class="col-md-6">
                             <label class="labels">Name</label>
-                            <input type="text" class="form-control"  value="{{ Auth::user()->name }}">
+                            <input type="text" class="form-control" name="full_name" value="{{ Auth::user()->name }}">
+                            @error('full_name')
+                                <small class="text-danger font-bold">{{ $message }}</small>
+                            @enderror
                         </div>
                         <div class="col-md-6">
                             <label class="labels">Phone Number</label>
-                            <input type="tel" class="form-control"  value="">
+                            <input type="tel" name="phone_number" class="form-control" value="">
+                            @error('phone_number')
+                                <small class="text-danger font-bold">{{ $message }}</small>
+                            @enderror
                         </div>
                         <div class="col-md-6">
                             <label class="labels">Address</label>
-                            <textarea class="form-control"  ></textarea>
+                            <textarea class="form-control" name="address"></textarea>
+                            @error('address')
+                                <small class="text-danger font-bold">{{ $message }}</small>
+                            @enderror
                         </div>
 
                         <h3 class="col-12 my-5">Emergency Contact</h3>
                         <div class="col-md-6">
                             <label class="labels">Name</label>
-                            <input type="text" class="form-control"  value="">
+                            <input type="text" name="n_name" class="form-control" value="">
+                            @error('n_name')
+                                <small class="text-danger font-bold">{{ $message }}</small>
+                            @enderror
                         </div>
                         <div class="col-md-6">
                             <label class="labels">Phone Number</label>
-                            <input type="tel" class="form-control"  value="">
+                            <input type="tel" name="n_phone" class="form-control" value="">
+                            @error('n_phone')
+                                <small class="text-danger font-bold">{{ $message }}</small>
+                            @enderror
                         </div>
                     </div>
                     <div class="mt-5 text-center">
