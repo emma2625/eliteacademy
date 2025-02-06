5b90ae6eab9cfe2bc9773c3c5870810f641ea38e
emma2625 <emmanuelodobo10@gmail.com>
2025-02-06 16:59:24 +0100

    Updated Classes View and Controller

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -20,4 +20,17 @@ class PageController extends Controller
     {
         return view('contact');
     }
+
+    public function sendEmail(Request $request)
+    {
+        $data = $request->validate([
+            'name' => "required|string",
+            'email' => "required|email",
+            'subject' => "required|string",
+            'branch' => "required|string|in:kubwa,lekki,bristol",
+            'message' => "required"
+        ]);
+
+        dd($data);
+    }
 }
